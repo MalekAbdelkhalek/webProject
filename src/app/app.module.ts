fd844858446926c51c9959d1230cfc54b51e345e
malek <abdelkhalek.malek0000@gmail.com>
2023-11-16 00:40:17 +0100

    signup component

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,15 +4,12 @@ import { FormsModule } from '@angular/forms';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { LoginComponent } from './login/login.component';
-import { TestComponent } from './test/test.component';
-import { RegisterComponent } from './register/register.component';
+
 
 @NgModule({
   declarations: [
     AppComponent,
     LoginComponent,
-    TestComponent,
-    RegisterComponent
   ],
   imports: [
     BrowserModule,
